0ca9f95cd31b61412ff3dc8a23df317a95d098c1
HelloImDevion <devionfothergill@gmail.com>
2017-09-11 12:17:29 -0400

    Added Fixes

--- Week4VirtualPet/Wolf.cs
+++ Week4VirtualPet/Wolf.cs
@@ -57,13 +57,13 @@ namespace Week4VirtualPet
             get { return this.maxWaste; }
         }
 
-        public long Ticks { get; }
 
         public Wolf()
         {
 
 
         }
+        
 
         public Wolf(string wolfName)
         {
@@ -71,66 +71,78 @@ namespace Week4VirtualPet
             this.currentRest = 100;
             this.maxRest = 100;
             this.currentHunger = 20;
-            this.maxHunger = 20;
+            this.maxHunger = 50;
             this.currentThirst = 20;
-            this.maxThirst = 20;
-            this.currentWaste = 10;
-            this.maxWaste = 10;
+            this.maxThirst = 50;
+            this.currentWaste = 20;
+            this.maxWaste = 50;
 
         }
 
         public void MainMenu()
         {
-
             while (this.currentRest > 0)
             {
-
                 Console.WriteLine(" Health = " + this.currentRest);
                 Console.WriteLine(" Hunger = " + this.currentHunger);
                 Console.WriteLine(" Thirst = " + this.currentThirst);
                 Console.WriteLine(" Waste =  " + this.currentWaste);
-              
+
                 Console.WriteLine("Press 1 to Eat.");
                 Console.WriteLine("Press 2 to Drink.");
                 Console.WriteLine("Press 3 to Use The Bathroom.");
                 Console.WriteLine("Press 4 to Sleep.");
+                Console.WriteLine("Press 5 to Play. ");
 
                 int userEnteredNumber = int.Parse(Console.ReadLine());
 
-                if (userEnteredNumber == 1)
+
+                if (this.currentHunger <= 0 || this.currentRest <= 0 || this.currentThirst <= 0 || this.currentWaste <= 0 || this.currentRest <= 0)
                 {
-                    this.currentHunger = this.maxHunger;
-                    this.currentThirst -= 2;
-                    this.currentWaste -= 2;
+                    Console.WriteLine("Your Wolf Died.");
+                    break;
+                }
+
+
+                else if (userEnteredNumber == 1)
+                {
+                    this.currentHunger += 10;
+                    this.currentThirst -= 5;
+                    this.currentWaste -= 5;
                     this.currentRest -= 10;
                     Console.WriteLine("Im stuffed!!");
                 }
                 else if (userEnteredNumber == 2)
                 {
-                    this.currentThirst = maxThirst;
-                    this.currentHunger -= 2;
-                    this.currentWaste -= 2;
+                    this.currentThirst += 10;
+                    this.currentHunger -= 5;
+                    this.currentWaste -= 5;
                     this.currentRest -= 10;
                     Console.WriteLine("Refreshing!!");
                 }
                 else if (userEnteredNumber == 3)
                 {
-                    this.currentWaste = maxWaste;
-                    this.currentHunger -= 2;
-                    this.currentThirst -= 2;
+                    this.currentWaste += 10;
+                    this.currentHunger -= 5;
+                    this.currentThirst -= 5;
                     this.currentRest -= 10;
                     Console.WriteLine("Ewww I think you should clean that up.");
-                   
+
                 }
                 else if (userEnteredNumber == 4)
                 {
                     this.currentRest = this.maxRest;
-                    this.currentHunger -= 2;
-                    this.currentThirst -= 2;
-                    this.currentWaste -= 2;
+                    this.currentHunger -= 5;
+                    this.currentThirst -= 5;
+                    this.currentWaste -= 5;
                     Console.WriteLine("Ready for the day!!");
                 }
+
+                else
+                {
+                    Console.WriteLine("Please use the given options.");
+                }
             }
-        }
+        }      
     }
 }
